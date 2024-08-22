9dbf2589adf7e5ede0ee42ad004940b9e61a4b1b
Mar4inja <mar4inja19@gmail.com>
2024-08-22 15:53:06 +0200

    second layer

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./home.module.css"; // Importē CSS moduli
 import MultiCardCarousel from "../card-carousel/MultiCardCarousel";
-import discountImage from "../../img/emp-discounts.png";
-// import buyPc from "../../img/buyPC.png";
-// import buyLaptop from "../../img/buyLaptop.png";
-// import buyTablet from "../../img/buyTablet.png";
+import discountImage from "../../img/quality.png";
+import pc from "../../img/buyPC.png";
+import laptop from "../../img/buyLaptop.png";
+import tablets from "../../img/buyTablet.png";
 import andGetVRam from "../../img/andGetVRAM.png"
 
 
@@ -19,9 +19,27 @@ const Home: React.FC = () => {
         <img className={styles["discount_icon"]} src={discountImage} alt="Discount" />
       </div>
       <div className={styles["second-content-container"]}>
-        {/* <img className={styles["pc"]} src={buyPc} alt="PC" />
-        <img className={styles["laptop"]} src={buyLaptop} alt="LAPTOP" />
-        <img className={styles["tablet"]} src={buyTablet} alt="TABLET" /> */}
+        <div className={styles["card_container"]}>
+          <div className={styles["card_one"]}>
+            <h3>Tablets</h3>
+            <img className={styles["tablets"]} src={tablets} alt="Tablets" />
+          </div>
+          <div className={styles["card_two"]}>
+            <img className={styles["laptop"]} src={laptop} alt="Laptop" />
+          </div>
+          <div className={styles["card_three"]}>
+
+          </div>
+          <div className={styles["card_four"]}>
+            <img className={styles["pc"]} src={pc} alt="Pc" />
+          </div>
+          <div className={styles["card_five"]}>
+
+          </div>
+          <div className={styles["card_six"]}>
+
+          </div>
+        </div>
         <img className={styles["vram"]} src={andGetVRam} alt="VRAM" />
       </div>
       <div className={styles["third-content-container"]}>
